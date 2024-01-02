8a556e6c613e6100d053896c39eba351b1ba0399
Virginie Dourson <virginiedourson@gmail.com>
2024-01-02 14:02:10 +0100

    add post doctor

--- src/main/java/org/hospital/hospitalregistrationapi/controllers/HospitalRegistrationsController.java
+++ src/main/java/org/hospital/hospitalregistrationapi/controllers/HospitalRegistrationsController.java
@@ -1,7 +1,12 @@
 package org.hospital.hospitalregistrationapi.controllers;
 
+import org.hospital.hospitalregistrationapi.entities.Doctor;
 import org.hospital.hospitalregistrationapi.repositories.DoctorsRepository;
 import org.hospital.hospitalregistrationapi.repositories.VisitsRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,6 +42,12 @@ public class HospitalRegistrationsController {
 	
 //	Virginie
 	
+	@PostMapping("/api/doctors")
+	public ResponseEntity<Integer> create(@RequestBody Doctor doctor) {
+		doctors.addNewDoctor(doctor);
+		System.out.println(doctor.getName());
+		return new ResponseEntity<Integer>((Integer) doctor.getId(), HttpStatus.CREATED);
+	}
 	
 	
 	
