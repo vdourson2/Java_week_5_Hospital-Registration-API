72bb51c4b73719c873b72163b2643a754d74252a
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-02 09:32:11 +0100

    add project

---
+++ Hospital-API/src/main/java/org/hospital/hospitalapi/HospitalApiApplication.java
@@ -0,0 +1,13 @@
+package org.hospital.hospitalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApiApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(HospitalApiApplication.class, args);
+  }
+  
+}
---
+++ Hospital-API/src/test/java/org/hospital/hospitalapi/HospitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hospital.hospitalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApiApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
