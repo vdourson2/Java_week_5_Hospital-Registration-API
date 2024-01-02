68856859fa0b5f12a43ca024689380bea7422cfc
Virginie Dourson <virginiedourson@gmail.com>
2024-01-02 10:30:58 +0100

    Create void controller

---
+++ src/main/java/controllers/HopitalRegistrationsController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HopitalRegistrationsController {
+
+//	Justine
+	
+	
+	
+	
+	
+	
+	
+	
+//	Valentin
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	Virginie
+	
+	
+	
+	
+	
+	
+	
+}
