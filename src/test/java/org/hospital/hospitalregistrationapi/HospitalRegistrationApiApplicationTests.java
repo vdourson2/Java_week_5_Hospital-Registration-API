f2ef1f4dd4237fbc9f5b02fea49054d2ed08e8b7
Valentin-Lefort <lefortvalentin94@gmail.com>
2024-01-02 09:34:22 +0100

    add project

---
+++ src/test/java/org/hospital/hospitalregistrationapi/HospitalRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hospital.hospitalregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistrationApiApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
